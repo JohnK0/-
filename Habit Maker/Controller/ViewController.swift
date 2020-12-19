da8bddd90d91e7b129f1b36e831fcfd12d68efff
JohnK0 <johnskim97@gmail.com>
2020-12-18 23:57:24 -0800

    Created layout for the habit creation page Created outlets and action functions for the views in the creation page

--- Habit Maker/Controller/ViewController.swift
+++ Habit Maker/Controller/ViewController.swift
@@ -8,17 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var table: UITableView?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var habitNameTextBox: UITextField!
+    @IBOutlet weak var habitTrajectoryTypes: UISegmentedControl!
+    @IBOutlet weak var habitMeasurementTypes: UISegmentedControl!
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+  
+    
+    
+    @IBAction func edittedHabitName(_ sender: UITextField) {
     }
     
-    func tableViewCreate() {
-        table! = UITableView()
+    @IBAction func edittedHabitTypes(_ sender: UISegmentedControl) {
     }
-
+    
+    @IBAction func submittedHabit(_ sender: UIButton) {
+    }
     
 
 }
---
+++ Habit Maker/Model/CalculateBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculateBrain.swift
+//  BMI Calculator
+//
+//  Created by John Kim on 12/16/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
