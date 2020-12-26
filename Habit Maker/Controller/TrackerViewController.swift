6b982bce18937464266862d72dff9ce11f69f6ce
JohnK0 <johnskim97@gmail.com>
2020-12-25 23:03:00 -0800

    Add trackerViewController for tracker storyboard

---
+++ Habit Maker/Controller/TrackerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrackerViewController.swift
+//  Habit Maker
+//
+//  Created by John Kim on 12/25/20.
+//
+
+import UIKit
+
+class TrackerViewController: UIViewController {
+
+    var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
